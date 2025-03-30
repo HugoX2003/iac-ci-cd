ec7f72c30d5c55cef470410e01f5661e6d492f34
HugoX2003 <hmarquezd1@gmail.com>
2025-03-30 19:43:05 +0000

    fix: import de app para testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,12 @@
+import sys
+import pathlib
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.resolve()))
+
+from app import app
+
 def test_home():
-    from app import app
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Flask" in response.data  # Asegura que tu mensaje de bienvenida lo tenga
+    assert b"Flask" in response.data
